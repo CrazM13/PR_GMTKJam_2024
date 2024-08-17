07bd8f1c1e4aafe2eb9ea58def0b786d280c9637
CrazM13 <sepulvedajoey13@yahoo.com>
2024-08-17 16:58:51 -0400

    Fixed WASD

--- Scripts/Player/PlayerMovement.cs
+++ Scripts/Player/PlayerMovement.cs
@@ -46,7 +46,7 @@ public partial class PlayerMovement : Node2D {
 		if (acceptInputs) {
 			// Get the input direction and handle the movement/deceleration.
 			// As good practice, you should replace UI actions with custom gameplay actions.
-			Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+			Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 			if (direction != Vector2.Zero) {
 
 				float newX = direction.X * Speed;
