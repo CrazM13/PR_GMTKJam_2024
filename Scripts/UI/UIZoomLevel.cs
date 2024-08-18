f047191cee4e7dfad2fc2b791fb13b1cd1d82148
CrazM13 <sepulvedajoey13@yahoo.com>
2024-08-18 11:09:28 -0400

    Improved Zoom UI

--- Scripts/UI/UIZoomLevel.cs
+++ Scripts/UI/UIZoomLevel.cs
@@ -4,27 +4,38 @@ using System;
 public partial class UIZoomLevel : Label {
 
 	[Export] private PlayerMovement player;
+	[Export] private float zoomSpeed;
 
-	private string[] zoomScales = {
-		"10^-14.5",
-		"10^-3.3",
-		"10^-0.7",
-		"10^0.7",
-		"10^2.7",
-		"10^3.8",
-		"10^4.6",
-		"10^6.5",
-		"10^7.1",
-		"10^8.3",
-		"10^9.1",
-		"10^9.7",
-		"10^11.0",
-		"10^12.5",
-		"10^13.3"
+	private float[] zoomScales = {
+		-14.5f,
+		-3.3f,
+		-0.7f,
+		0.7f,
+		2.7f,
+		3.8f,
+		4.6f,
+		6.5f,
+		7.1f,
+		8.3f,
+		9.1f,
+		9.7f,
+		11.0f,
+		12.5f,
+		13.3f
 	};
 
+	private float currentScale = 0f;
+	private float targetScale = 0f;
+
 	private void OnLevelUp() {
-		this.Text = $"Zoom: {zoomScales[player.GetFormIndex]}";
+		targetScale = zoomScales[player.GetFormIndex];
+	}
+
+	public override void _Process(double delta) {
+		base._Process(delta);
+
+		currentScale = Mathf.MoveToward(currentScale, targetScale, (float) delta * zoomSpeed);
+		this.Text = currentScale.ToString("F1");
 	}
 
 }
