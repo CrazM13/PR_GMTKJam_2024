c239d3d47663fc858db1cc0c9c09f35c1fb1b7d1
CrazM13 <sepulvedajoey13@yahoo.com>
2024-08-18 10:55:02 -0400

    Added Zoom UI

--- Scripts/UI/UIZoomLevel.cs
+++ Scripts/UI/UIZoomLevel.cs
@@ -6,21 +6,21 @@ public partial class UIZoomLevel : Label {
 	[Export] private PlayerMovement player;
 
 	private string[] zoomScales = {
-		"TINY",
-		"LESS TINY",
-		"SMALL",
-		"MEH",
-		"LARGE",
-		"VERY LARGE",
-		"OKAY",
-		"BIG ENOUGH",
-		"STOP",
-		"OK REALLY",
-		"STOOOP",
-		"TOO BIG",
-		"U FAT",
-		"U REALLY FAT",
-		"YO MAMA"
+		"10^-14.5",
+		"10^-3.3",
+		"10^-0.7",
+		"10^0.7",
+		"10^2.7",
+		"10^3.8",
+		"10^4.6",
+		"10^6.5",
+		"10^7.1",
+		"10^8.3",
+		"10^9.1",
+		"10^9.7",
+		"10^11.0",
+		"10^12.5",
+		"10^13.3"
 	};
 
 	private void OnLevelUp() {
